9340083f2fb5c0ef266d31a87cac8b571cc3421f
Vitaliy <vit4ly.kiselev@gmail.com>
2020-08-18 21:18:13 +0300

    Task inter

---
+++ src/views/List.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1>List</h1>
+
+  <hr>
+  <table v-if="tasks.length">
+
+  </table>
+<p v-else>No Tasks</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "List.vue",
+  computed: {
+    tasks() {
+      return this.$store.getters.tasks
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Task.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Task</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "Task.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
